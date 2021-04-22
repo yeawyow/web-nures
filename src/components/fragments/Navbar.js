2974bdcbdd9ecf474f3e3c4f249c3a3f245a2e83
[yeawyow] <[yeawyow@gmail.com]>
2021-04-22 21:47:58 +0700

    menu resposive complaete

--- src/components/fragments/Navbar.js
+++ src/components/fragments/Navbar.js
@@ -22,9 +22,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   grow: { flexGrow: 1 },
-  menuButton: {
-    marginRight: theme.spacing(10),
-  },
+
   title: {
     display: "none",
     [theme.breakpoints.up("sm")]: {
@@ -51,79 +49,20 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 18,
     margin: theme.spacing(3),
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
 }));
 
 function NavBar() {
   const classes = useStyles();
   const history = useHistory();
-  const menuId = "primary-search-account-menu";
-  const [isMenuOpen, setisMenuOpen] = useState(false);
-  const [isMobileMenuOpent, setisMobileMenuOpent] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  const renderMenu = (
-    <Menu
-      anchorEl={isMenuOpen}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      // onClose={handleMenuClose}
-    >
-      <MenuItem
-      // onClick={this.handleMenuClose}
-      >
-        Profile
-      </MenuItem>
-      <MenuItem
-      // onClick={this.handleMenuClose}
-      >
-        My account
-      </MenuItem>
-    </Menu>
-  );
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      // anchorEl={this.state.mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      // open={isMobileMenuOpen}
-      // onClose={this.handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem
-      // onClick={this.handleProfileMenuOpen}
-      >
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const navLinks = [
     { title: `หน้าหลัก`, path: `/` },
     { title: `ข้อมูลหน่วยงาน`, path: `/about` },
@@ -132,21 +71,35 @@ function NavBar() {
     { title: `ดาวน์โหลดเอกสาร`, path: `/contact` },
     { title: `faq`, path: `/faq` },
   ];
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id="long-menu"
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={open}
+      onClose={handleClose}
+    >
+      {navLinks.map(({ title, path }) => (
+        <MenuItem
+          component={NavLink}
+          onClick={handleClose}
+          to={`${process.env.PUBLIC_URL}` + `${path}`}
+        >
+          <p>{title}</p>
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
-    <div className={classes.grow}>
+    <div>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-            // onClick={this.props.openDrawerHandler}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             {navLinks.map(({ title, path }) => (
               <Button
@@ -163,13 +116,14 @@ function NavBar() {
           <div className={classes.sectionMobile}>
             <IconButton
               aria-label="show more"
-              aria-controls={mobileMenuId}
+              //  aria-controls={mobileMenuId}
               aria-haspopup="true"
-              // onClick={this.handleMobileMenuOpen}
+              onClick={handleClick}
               color="inherit"
             >
-              <MoreIcon />
+              <MenuIcon />
             </IconButton>
+            {renderMobileMenu}
           </div>
         </Toolbar>
       </AppBar>
